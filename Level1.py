1dfb62e0ccb44eeff4d9410f4d128a63992ae9c9
VVP42 <70865113+VVP42@users.noreply.github.com>
2020-09-08 01:08:11 +0300

    Add files via upload

--- Level1.py
+++ Level1.py
@@ -1,7 +1,21 @@
-import math
 
 
-def  squirrel(N): 
-    k=  len(str(math.factorial(N)))
-    return math.factorial(N) // (10 ** (k-1))
+
+
+def odometer(arr):
     
+     length =0
+    
+     
+     Diff_Hr = 0
+     i=0
+     while i< len(arr):
+       if (i % 2 ==0):
+           Current_Km = arr[i]
+       else:
+           Current_Hr = arr[i]-Diff_Hr
+           length+= Current_Hr*Current_Km
+           Diff_Hr = arr[i]
+       i+=1
+     return length
+
