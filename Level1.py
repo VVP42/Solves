57cf71a142fc0b9ba293d0da6955a6bc15cd720e
VVP42 <70865113+VVP42@users.noreply.github.com>
2020-09-24 01:05:53 +0300

    Add files via upload

--- Level1.py
+++ Level1.py
@@ -2,20 +2,53 @@
 
 
 
-def odometer(arr):
+def ConquestCampaign(N,M,L, battalion):          
+ 
+    matrix = [[0] * M for i in range(N)]
+    ind=0
+    k=0
+    f=0
+    while (ind<2*(L)):
+        k=battalion[ind]
+        f = battalion[ind+1]
+        matrix[k-1][f-1]=1
+        ind+=2
+    print(matrix)
+        
     
-     length =0
     
-     
-     Diff_Hr = 0
-     i=0
-     while i< len(arr):
-       if (i % 2 ==0):
-           Current_Km = arr[i]
-       else:
-           Current_Hr = arr[i]-Diff_Hr
-           length+= Current_Hr*Current_Km
-           Diff_Hr = arr[i]
-       i+=1
-     return length
+    count =1
+       
+    def is_zero(matrix,N,M):
+        for i in range(N): 
+          for j in range(M): 
+             if (matrix[i][j]==0):
+                 return 0
+
+    while (is_zero(matrix,N,M)==0):
+        
+        for i in range(N): 
+          for j in range(M): 
+            if (matrix[i][j]>0):
+                matrix[i][j]+=1
+
+        for i in range(N): 
+          for j in range(M): 
+            if  (matrix[i][j]>=2):
+                if (i>0 and matrix[i-1][j]==0):
+                    matrix[i-1][j]+=1
+                if (i<N-1 and matrix[i+1][j]==0):
+                    matrix[i+1][j]+=1
+                if (j>0 and matrix[i][j-1]==0):
+                    matrix[i][j-1]+=1
+                if (j<M-1 and matrix[i][j+1]==0):
+                    matrix[i][j+1]+=1
+        count+=1
+    
+             
+    
+    return count
+
+
+
 
