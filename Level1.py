96845ee73126f9b110b2d29a7ce61683ead2d5aa
VVP42 <70865113+VVP42@users.noreply.github.com>
2020-09-29 20:22:44 +0300

    Add files via upload

--- Level1.py
+++ Level1.py
@@ -1,54 +1,20 @@
 
+def MadMax(N, Tele):
+   
+    exchange=0
+    for j in range(N):
+     for i in range(N-1):
+        if (Tele[i]>Tele[i+1]):
+            exchange=Tele[i+1]
+            Tele[i+1]=Tele[i]
+            Tele[i]=exchange
+    exchange=0
+    print(Tele)
+      
+    for i in range(int((N-1)/4)):
+            exchange=Tele[i+int((N-1)/2)]
+            Tele[i+int((N-1)/2)] = Tele[N-(i+1)]
+            Tele[N-(i+1)] = exchange
+            
 
-
-
-def ConquestCampaign(N,M,L, battalion):          
- 
-    matrix = [[0] * M for i in range(N)]
-    ind=0
-    k=0
-    f=0
-    while (ind<2*(L)):
-        k=battalion[ind]
-        f = battalion[ind+1]
-        matrix[k-1][f-1]=1
-        ind+=2
-    print(matrix)
-        
-    
-    
-    count =1
-       
-    def is_zero(matrix,N,M):
-        for i in range(N): 
-          for j in range(M): 
-             if (matrix[i][j]==0):
-                 return 0
-
-    while (is_zero(matrix,N,M)==0):
-        
-        for i in range(N): 
-          for j in range(M): 
-            if (matrix[i][j]>0):
-                matrix[i][j]+=1
-
-        for i in range(N): 
-          for j in range(M): 
-            if  (matrix[i][j]>=2):
-                if (i>0 and matrix[i-1][j]==0):
-                    matrix[i-1][j]+=1
-                if (i<N-1 and matrix[i+1][j]==0):
-                    matrix[i+1][j]+=1
-                if (j>0 and matrix[i][j-1]==0):
-                    matrix[i][j-1]+=1
-                if (j<M-1 and matrix[i][j+1]==0):
-                    matrix[i][j+1]+=1
-        count+=1
-    
-             
-    
-    return count
-
-
-
-
+    return Tele
