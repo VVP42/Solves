493bacc12b38e1a124f81f59ae4d11f50a73df55
VVP42 <70865113+VVP42@users.noreply.github.com>
2020-11-02 01:10:45 +0300

    Add files via upload

--- Level1.py
+++ Level1.py
@@ -1,23 +1,36 @@
+import math
 
 
-def SynchronizingTables(N, ids,salary):
+def PatternUnlock(N,hits):
    
-    exchange=0
+    arr_index=[]
+    arr_search = [[6,1,9],[5,2,8],[4,3,7]]
+    length =  float(0)
+    
+    for i in range(N):
+        current_number = hits[i]
+        for k in range(3):
+            for j in range(3):
+                if (arr_search[k][j] == current_number):
+                  arr_index.append(k)
+                  arr_index.append(j)
+    
     i=0
-    j=0
+    while (i < len(arr_index) -3):
+        if (abs(arr_index[i+2] - arr_index[i]) ==abs(arr_index[i+3] - arr_index[i+1]) ):
+            length =length+math.sqrt(2)
+
+        else:
+            length=length+float(1)
+        i+=2
+    length= round(length,5)    
+    
+    str_numb=str(length)
+    bad_chars =['.', '0']
+    for i in bad_chars : 
+        str_numb = str_numb.replace(i, '') 
     
     
-    for j in range(N):
-             
-        for i in range(N):
-            if ( (ids[j] > ids[i] and salary[j] < salary[i]) or (ids[j] < ids[i] and salary[j] > salary[i]) ):
-                exchange = salary[i]
-                salary[i] = salary[j]
-                salary[j] = exchange
-           
-            
-    
-      
-   
-    return salary
+    return str_numb
+
 
