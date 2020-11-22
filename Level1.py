dea80bcf85fff34c9b427bc58363a308dde5861e
VVP42 <70865113+VVP42@users.noreply.github.com>
2020-11-22 21:24:19 +0300

    Add files via upload

--- Level1.py
+++ Level1.py
@@ -1,36 +1,75 @@
-import math
 
+import textwrap as tw
+import re
+ 
 
-def PatternUnlock(N,hits):
+def WordSearch(leng, s, subs): 
    
-    arr_index=[]
-    arr_search = [[6,1,9],[5,2,8],[4,3,7]]
-    length =  float(0)
-    
-    for i in range(N):
-        current_number = hits[i]
-        for k in range(3):
-            for j in range(3):
-                if (arr_search[k][j] == current_number):
-                  arr_index.append(k)
-                  arr_index.append(j)
-    
-    i=0
-    while (i < len(arr_index) -3):
-        if (abs(arr_index[i+2] - arr_index[i]) ==abs(arr_index[i+3] - arr_index[i+1]) ):
-            length =length+math.sqrt(2)
+   words = s.split()
+   
+   
+   new_string =''
+   now_str =''
+   for i in range(len(words)):
+       if len(words[i]) < leng and len(now_str) + len(words[i]) < leng:
+           new_string+=' '
+           now_str+=' '
+           new_string += words[i]
+           now_str+=words[i]
+           
+           continue
+       if len(words[i]) < leng and len(now_str) + len(words[i]) > leng:
+           now_str=''
+           new_string+='\n '
+           new_string += words[i]
+           now_str+= words[i]
+           continue
+       
+       if len(words[i]) >= leng:
+           j =0
+           k=0
+           while j< len(words[i]):
+            now_str=''
+            new_string +='\n'
+            new_string +=words[i][j:leng+k]
+            
+            j+=leng
+            k+=leng
+            #new_string+= words[i][leng+1:len(words[i])]
+            #now_str+= words[i][leng:len(words[i])]
+            #continue
+           new_string +='\n'
+       
+       if len(now_str) >= leng:
+           now_str=''
+           new_string +='\n'
+           new_string+=words[i]
+           now_str+=words[i]
+           continue
+       
+       
 
-        else:
-            length=length+float(1)
-        i+=2
-    length= round(length,5)    
-    
-    str_numb=str(length)
-    bad_chars =['.', '0']
-    for i in bad_chars : 
-        str_numb = str_numb.replace(i, '') 
-    
-    
-    return str_numb
+       word_sostavn=''
+   str_search = new_string.split('\n')
+   
+   
+   mass =list()
+   current_number=0
+   for i in range(len(str_search)):
+       for j in range(len(str_search[i].split())):
+         
+         if  subs ==  str_search[i].split()[j]:
+            current_number=1
+            break
+         else:
+            current_number=0
+       mass.append(current_number)
+
+   
+   return mass
+   
+
+   
+
 
 
